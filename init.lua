aba346162e3a7c79224260e97d16bb363241c584
OrkanGökcen <orkan.gok@gmail.com>
2025-12-18 15:24:05 +0100

    Deleted Format_on_save

--- init.lua
+++ init.lua
@@ -372,17 +372,6 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         csharp = { 'csharpier' },
