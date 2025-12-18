832b2c6bfa7cf3e1b6319c8b93b1c2f92c2cc4ab
OrkanGökcen <orkan.gok@gmail.com>
2025-12-18 15:27:15 +0100

    Editing of treesitter (not done)

--- init.lua
+++ init.lua
@@ -451,18 +451,9 @@ require('lazy').setup({
     end,
   },
   {
-    'nvim-treesitter/nvim-treesitter',
+    'nvim-jreesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
   },
 
   require 'kickstart.plugins.indent_line',
